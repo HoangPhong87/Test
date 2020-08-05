1920c6e265d076660eacf4437da98743767d5af1
HoangPhong87 <giaphoangphong@gmail.com>
2020-08-05 07:54:56 +0700

    QuanLyNhanSu

---
+++ QuanLyNhanSu/QuanLyNhanSu/Controllers/NhanSuController.cs
@@ -0,0 +1,73 @@
+﻿using QuanLyNhanSu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyNhanSu.Controllers
+{
+    public class NhanSuController : Controller
+    {
+        // GET: NhanSu
+        public ActionResult Index()
+        {
+            DsNhanSu danhsach = new DsNhanSu();
+            List<NhanSu> obj = danhsach.getnhansu(string.Empty).OrderBy(x => x.FullName).ToList();
+            return View(obj);
+        }
+        //them phuong thuc Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(NhanSu ns)
+        {
+            if (ModelState.IsValid)
+            {
+                DsNhanSu danhsach = new DsNhanSu();
+                danhsach.AddNhanSu(ns);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Them phuong thuc Edit
+        public ActionResult Edit(string id = "")
+        {
+            DsNhanSu danhsach = new DsNhanSu();
+            List<NhanSu> obj = danhsach.getnhansu(id);
+            return View(obj.FirstOrDefault());// lay ban ghi dau
+        }
+        [HttpPost]
+        public ActionResult Edit (NhanSu ns)
+        {
+            DsNhanSu danhsach = new DsNhanSu();
+            danhsach.UpdateNhanSu(ns);
+            return RedirectToAction("Index");
+        }
+
+        //phuong thuc Details
+        public ActionResult Details(string id = "")
+        {
+            DsNhanSu danhsach = new DsNhanSu();
+            List<NhanSu> obj = danhsach.getnhansu(id);
+            return View(obj.FirstOrDefault());
+        }
+        // Phuong thuc Delete
+        public ActionResult Delete (string id = "")
+        {
+            DsNhanSu danhsach = new DsNhanSu();
+            List<NhanSu> obj = danhsach.getnhansu(id);
+            return View(obj.FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Delete(NhanSu ns)
+        {
+            DsNhanSu danhsach = new DsNhanSu();
+            danhsach.DeleteNhanSu(ns);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QuanLyNhanSu/QuanLyNhanSu/Models/NhanSu.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyNhanSu.Models
+{
+    public class NhanSu
+    {
+        public int ID { set; get; }
+        public string FullName { set; get; }
+        public string Address { set; get; }
+        public string Phone { set; get; }
+        public string Email { set; get; }
+    }
+    class DsNhanSu
+    {
+        DBConnection db;
+        public DsNhanSu()
+        {
+            db = new DBConnection();
+        }
+        //lay ve ds nhan vien
+        public List<NhanSu> getnhansu(string ID)
+        {
+            string sql;
+            if (string.IsNullOrEmpty(ID))
+                sql = "SELECT * FROM NhanSu";
+            else
+                sql = " SELECT * FROM NhanSu WHERE Id=" + ID;
+            List<NhanSu> danhsach = new List<NhanSu>();
+            DataTable dt = new DataTable();
+            SqlConnection con = db.getConnection();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            con.Open();
+            da.Fill(dt);
+            da.Dispose();
+            con.Close();
+            NhanSu tmpNS;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                tmpNS = new NhanSu();
+                tmpNS.ID = Convert.ToInt32(dt.Rows[i]["ID"].ToString());
+                tmpNS.FullName = dt.Rows[i]["FullName"].ToString();
+                tmpNS.Address = dt.Rows[i]["Address"].ToString();
+                tmpNS.Phone = dt.Rows[i]["Phone"].ToString();
+                tmpNS.Email = dt.Rows[i]["Email"].ToString();
+                danhsach.Add(tmpNS);
+            }
+            return danhsach;
+        }
+        //Them du lieu nv
+        public void AddNhanSu(NhanSu ns)
+        {
+            string sql = " INSERT INTO NhanSu(FullName,Address,Phone,Email) VALUES (N'" + ns.FullName + "',N'" + ns.Address + "','" + ns.Phone + "','" + ns.Email + "')";
+            SqlConnection con = db.getConnection();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        //Sua du lieu 
+        public void UpdateNhanSu(NhanSu ns)
+        {
+            string sql = " UPDATE NhanSu SET FullName =N'" + ns.FullName + "',Address=N'" + ns.Address + "',Phone='" + ns.Phone + "',Email='" + ns.Email + "' WHERE ID= " + ns.ID;
+            SqlConnection con = db.getConnection();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+
+        //Xoa du lieu
+        public void DeleteNhanSu(NhanSu ns)
+        {
+            string sql = " DELETE NhanSu WHERE ID =" + ns.ID;
+            SqlConnection con = db.getConnection();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+    }
+}
